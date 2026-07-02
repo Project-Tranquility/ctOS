972a43150a91d4778db396512e497afc7b9e44e3
Lucas <lucas1.eeckhoutte@epitech.eu>
2026-07-02 10:17:56 +0200

    feat!: add first command help

--- cpOS.py
+++ cpOS.py
@@ -1,6 +1,22 @@
+#!/usr/bin/env python3
 import requests
+from test import demo
+from src.start.start import start
+from src.help import help
+import sys
 
 def main():
-    print("Hello from cpOS")
+    nb = len(sys.argv)
+    
+    if nb <= 1:
+        help()
+        return
+    if sys.argv[1] == "--help" or "-h":
+        help()
+    if sys.argv[1] == "hello":
+        demo()
+        start()
+    else:
+        return 84
 
 main()
