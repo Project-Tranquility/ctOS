223d719f6ec48b149407d7022e63117ee3efddb3
Lucas <lucas1.eeckhoutte@epitech.eu>
2026-07-02 10:35:20 +0200

    docs: add docs for command

--- src/help.py
+++ src/help.py
@@ -2,3 +2,6 @@ def help():
     print("Welcome to cpOS\n")
     print("Usage : cpOS [command]")
     print("        cpOS [flags]\n")
+    print("[command] :\n")
+    print(" --light toggle      (toggle connected light)")
+    print(" --music             (use command for music)")
